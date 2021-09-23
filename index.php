640aa5379e1c221a4ae3b179a06fbb250da789ad
Akhmad Herdian <akhmad.herdian@gmail.com>
2021-09-23 11:14:27 +0800

    Show basic data from file excel

--- index.php
+++ index.php
@@ -1,9 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Read Excel By PHPExcel</title>
+    <title>Read Excel By PHP Excel</title>
 </head>
 <body>
+    <center>
+        <h2>Read Excel By PHP Excel</h2>
 
+        <?php
+            require_once "ClassesPhpExcel/PHPExcel.php";
+            $path = "Test.xlsx";
+            $reader = PHPExcel_IOFactory::createReaderForFile($path);
+            $excelObj = $reader->load($path);
+
+            $worksheet = $excelObj->getSheet('0');
+
+            echo $worksheet->getCell('A2')->getValue();
+
+            $lastRow = $worksheet->getHighestRow();
+            $columnCount = $worksheet->getHighestDataColumn();
+
+            echo $lastRow;
+            echo $columnCount;
+        ?>
+
+    </center>
 </body>
 </html>
